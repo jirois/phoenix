2ee82316f694fc37a50e6681b01e6137570fb84d
Lin <omasajiri@gmail.com>
2021-08-13 12:50:39 +0100

    feat:setup files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div class="flex flex-row bg-green-800">
+      <div className="bg-red-400 py-4 px-8 m-2 text-white font:semi-bold">1</div>
+      <div className="bg-red-400 py-4 px-8 m-2 m-2 text-white">2</div>
+      <div className="bg-red-400 py-4 px-8 m-2 m-2 text-white">3</div>
+    </div>
+
+  );
+}
+
+export default App;
