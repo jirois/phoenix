fafd1ae803592e96e78d69bd05be67d18065a444
Lin <omasajiri@gmail.com>
2021-08-19 17:26:07 +0100

    fix: nav setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import HomeScreen from './screens/HomeScreen';
 import ServiceScreen from './screens/ServiceScreen';
 
@@ -8,8 +9,9 @@ function App() {
    
     <BrowserRouter>
     <div className="grid grid-areas-layout grid-cols-layout grid-rows-layout h-full">
-      <header className="grid-in-header row-start-header row-end-header bg-green-200 p-8">
-       <h1>Heading bar</h1>
+      <header className="grid-in-header row-start-header row-end-header">
+        <Navbar />
+      
       </header>
       <main className="grid-in-main row-start-main row-end-main">
         <Route path="/product" component={ServiceScreen} />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,28 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
-      gridTemplateAreas: {
-        'layout': [
-          'header',
-          'main',
-          'footer',
-        ],
-      },
-      gridTemplateColumns: {
-        'layout': '1fr'
-    },
-    gridTemplateRows: {
-      'layout': '5rem 1fr 5rem',
-    },
-
-  variants: {
-    extend: {
-      gridTemplateAreas: ['responsive']
-    },
+    extend: {},
   },
-  plugins: [
-    require('@savvywombat/tailwindcss-grid-areas')
-  ],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
