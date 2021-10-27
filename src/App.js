0a282e95c05725e4894744526ae33ba82765ab0f
Lin <omasajiri@gmail.com>
2021-10-27 22:47:51 +0100

    fea: folder and router dependencies setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom'
+import HomeScreen from './pages/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="header">
+       <h1>Header</h1>
       </header>
+    
+      <main className="main">
+        <div className="content">
+          <Route path="/" exact={true} component={HomeScreen} />
+        </div>
+      </main>
+      <footer className="footer">
+        All right reserved.
+  </footer>
     </div>
+  </BrowserRouter>
   );
 }
 
