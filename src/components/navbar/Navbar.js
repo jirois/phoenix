02d0f6f2d8717fea7242ff907f846d4c1ecfa13c
Lin <omasajiri@gmail.com>
2021-10-30 23:09:01 +0100

    fix: fix logo text

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,41 +1,39 @@
-import {links} from '../../data'
-import Button from '../button/Button';
-import { NavLink } from 'react-router-dom';
-import './navbar.css'
+import { links } from "../../data";
+import Button from "../button/Button";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar(props) {
+  return (
+    <>
+      <nav className="navbar  ">
+        <a className="menu-logo " href="/">
+          Welcome to <br /> phoenix cap
+        </a>
+        <div className="menu-btn ">
+          <Button />
+        </div>
 
-
-    return (
-        <>
-        <nav className="navbar  ">
-            <a className="menu-logo " href="/">
-                Welcome to phoenix cap
-            </a>
-            <div className="menu-btn ">
-    
-                <Button />
-            </div>
-
-            <ul className=" menu-item">
-            
-               {links.map((link) =>{
-                   const {id, url, text} = link
-                   return(
-                       <li key={id}>
-                            <NavLink exact to={url} className="text-desknav" activeClassName="activeClass" >{text}</NavLink>
-                       </li>
-                   )
-                    
-               })}
-    
-            </ul>
-           
-        </nav>
-        
-            
-        </>
-    );
+        <ul className=" menu-item">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <NavLink
+                  exact
+                  to={url}
+                  className="text-desknav"
+                  activeClassName="activeClass"
+                >
+                  {text}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
