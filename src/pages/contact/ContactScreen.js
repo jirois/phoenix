501a3dfc1e8c6d760d54cbb6d69453668d1f88a1
Omas Ajiri <omasajiri@gmail.com>
2021-11-10 16:44:03 +0100

    feat: added contacted and message forms

--- src/pages/contact/ContactScreen.js
+++ src/pages/contact/ContactScreen.js
@@ -3,10 +3,47 @@ import "./contact.css"
 
 function ContactScreen(props) {
     return (
-        <div className="service">
-            <h2>Contact page comming page!</h2>
+        <>
+            <div className="form-message">
+                <h2>Get in Touch</h2>
+                <form >
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Subject" />
+                    <textarea row="10" col="5" 
+                    placeholder="Type your messgage here..."></textarea>
+
+                    <button type="submit">Submit</button>
+
+                </form>
+            </div>
+            <div className="form-contact">
+                <h2>Contact Support</h2>
+                <form>
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="tel" placeholder="Phone" />
+                    <div className="dropdown">
+                    <input list="issues" />
+                    <datalist id="issues">
+                        <option value="Logging into account" />
+                        <option value="Completing Payment" />
+                        <option value="Viewing content" />
+                        <option value="Uploading files" />
+                        <option value="Other" />
+                    </datalist>
+                    </div>
+
+                    <textarea col="10" row="4" 
+                        placeholder="Let us know what you need help with..." ></textarea>
+                
+                    <button type="submit">Get in Touch</button>
+                </form>
+
+            </div>
             
-        </div>
+       </>
     );
 }
 
