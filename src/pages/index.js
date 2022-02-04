6c4c7dccff06046cd3ff261c881a4983d5b8cf6b
Omas Ajiri <omasajiri@gmail.com>
2022-02-04 08:39:45 +0100

    feat: added product and service section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/Hero';
+import Products from '../components/Product';
+import Services from '../components/Services';
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <Services />
+            <Products />
         </>
     )
 };
