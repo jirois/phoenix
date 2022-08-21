78edffb9d838bbe8b6389593fd56faa751c5e304
Omas Ajiri <omasajiri@gmail.com>
2022-08-21 17:12:29 +0100

    fix: fix  product page1

--- src/components/Services/ServiceElements.js
+++ src/components/Services/ServiceElements.js
@@ -62,7 +62,7 @@ export const ServicesIcon = styled.img`
 export const ServicesH1 = styled.h1`
   font-size: 2.5rem;
   color: #fff;
-  margin-bottom: 64px;
+  padding: 3rem 0;
 
   @media screen and (max-width: 480px) {
     font-size: 2rem;
