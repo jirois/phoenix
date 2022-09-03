98ac1c3e01bfe93c0afaab1c29ca59cf661ef547
Omas Ajiri <omasajiri@gmail.com>
2022-09-03 22:33:45 +0100

    feat: verify email fix4

--- src/pages/auth/VerifyEmail.js
+++ src/pages/auth/VerifyEmail.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import { HeaderFour, HeaderTwo, Page } from "../../components/Styles";
 // import { NavBtnLinkBtn } from "../../components/Navbar/navbarELements";
 import axios from "axios";
 import { baseUrl } from "../../utils/url";
 import { useGlobalContext } from "../../context";
 
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
+// function useQuery() {
+//   return new URLSearchParams(useLocation().search);
+// }
 const VerifyEmail = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
   const { loading: isLoading } = useGlobalContext();
-  const query = useQuery();
+  // const query = useQuery();
+  const [searchParams] = useSearchParams();
 
   const verifyToken = async () => {
     setLoading(true);
     try {
       const { data } = await axios.post(baseUrl + "auth/verify-email", {
-        verification: query.get("token"),
-        email: query.get("email"),
+        verification: searchParams.get("token"),
+        email: searchParams.get("email"),
       });
       console.log(data);
       setMessage(data.msg);
