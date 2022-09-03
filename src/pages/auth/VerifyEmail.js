7edd778742df6925992ac20464b4a6a47d7f8275
Omas Ajiri <omasajiri@gmail.com>
2022-09-04 00:29:22 +0100

    feat: working on the email1

--- src/pages/auth/VerifyEmail.js
+++ src/pages/auth/VerifyEmail.js
@@ -1,61 +1,61 @@
-// import React from "react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-// import { HeaderFour, HeaderTwo, Page } from "../../components/Styles";
+import { HeaderFour, HeaderTwo, Page } from "../../components/Styles";
 // import { NavBtnLinkBtn } from "../../components/Navbar/navbarELements";
-// import axios from "axios";
-// import { baseUrl } from "../../utils/url";
+import axios from "axios";
+import { baseUrl } from "../../utils/url";
+import { useState } from "react";
 // import { useGlobalContext } from "../../context";
 
 // function useQuery() {
 //   return new URLSearchParams(useLocation().search);
 // }
 const VerifyEmail = () => {
-  // const [error, setError] = useState(false);
-  // const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   // const { loading: isLoading } = useGlobalContext();
   // // const query = useQuery();
   const [searchParams] = useSearchParams();
   console.log(searchParams);
 
-  // const verifyToken = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const { data } = await axios.post(baseUrl + "auth/verify-email", {
-  //       verification: searchParams.get("token"),
-  //       email: searchParams.get("email"),
-  //     });
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(error.response.msg);
-  //     setError(true);
-  //   }
-  //   setLoading(false);
-  // };
+  const verifyToken = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.post(baseUrl + "auth/verify-email", {
+        verification: searchParams.get("token"),
+        email: searchParams.get("email"),
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error.response.msg);
+      setError(true);
+    }
+    setLoading(false);
+  };
 
-  // useEffect(() => {
-  //   if (!isLoading) {
-  //     verifyToken();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  useEffect(() => {
+    if (!loading) {
+      verifyToken();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  // if (loading) {
-  //   return (
-  //     <Page>
-  //       <HeaderTwo>Loading...</HeaderTwo>
-  //     </Page>
-  //   );
-  // }
-  // if (error) {
-  //   return (
-  //     <Page>
-  //       <HeaderFour>
-  //         There was an error, please double check your link
-  //       </HeaderFour>
-  //     </Page>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <Page>
+        <HeaderTwo>Loading...</HeaderTwo>
+      </Page>
+    );
+  }
+  if (error) {
+    return (
+      <Page>
+        <HeaderFour>
+          There was an error, please double check your link
+        </HeaderFour>
+      </Page>
+    );
+  }
 
   // return (
   //   <Page>
